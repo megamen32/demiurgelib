62ffaca6ec3084d6b9d63659c4203d55d1ea1f75
roomhackerfrom100 <megamen932@gmai.com>
2023-12-10 08:27:14 +0300

    v4.0.2a

--- safe_getter.py
+++ safe_getter.py
@@ -1,16 +1,17 @@
 import logging
 
-def try_get(func, default=None,return_exception=False):
-    res,err=try_gete(func,default)
+def try_get(func, default=None,return_exception=False,**kwargs):
+    res,err=try_gete(func,default,**kwargs)
     if return_exception and err is not None:
         return err
     return res
 
-def try_gete(func, default=None):
+def try_gete(func, default=None,ignore_exc=False):
     try:
         return func() or default,None
     except Exception as e :
-        logging.exception(e)
+        if not ignore_exc:
+            logging.exception(e)
         return default,e
 
 def try_geta(arr,func,skip_none=True,default_element=None):
@@ -27,7 +28,7 @@ def safe_lambdae(lambd, default=None):
     try:
         return lambd()
     except Exception:
-        loger.exception('lambda error:')
+        logging.exception('lambda error:')
         return default
 def apply_to_list(lst, func, filter_func=None,return_all=True):
     return [item for item in [try_get(func) for item in lst if filter_func is None or filter_func(item)] if return_all or item is not None ]
